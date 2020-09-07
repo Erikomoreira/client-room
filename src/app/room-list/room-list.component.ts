57b97af9b3de8ebfd562f2d6fdaa7a6aff71f402
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2020-09-07 20:05:50 -0300

    Ajustando o HTML e TS

--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -1,4 +1,9 @@
+import { RoomService } from './../room.service';
+import { RoomDetailsComponent } from './../room-details/room-details.component';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+import { Room } from "../room";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-room-list',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.css']
 })
 export class RoomListComponent implements OnInit {
+  rooms: Observable<Room[]>;
+  
+  constructor(private roomService: RoomService,
+    private router: Router) { }
 
-  constructor() { }
+  ngOnInit(){
+    this.reloadData();
+  }
 
-  ngOnInit(): void {
+  reloadData(){
+    this.rooms = this.roomService.getRoomList();
+  }
+
+  deleteRoom(id: number){
+    this.roomService.deleteRoom(id)
+    .subscribe(
+      data =>{
+        console.log(data)
+        this.reloadData();
+      },
+      error => console.log(error)
+    );
+  }
+
+  roomDetails(id: number){
+    this.router.navigate(['details', id]);    
+  }
+
+  updateRoom(id: number){
+    this.router.navigate(['update', id]);
   }
 
 }
