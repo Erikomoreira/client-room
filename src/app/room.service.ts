7c64e32ef025e0d6030d98f1aabe21f86b3fa466
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2020-09-07 22:23:53 -0300

    Ajuste Service

--- src/app/room.service.ts
+++ src/app/room.service.ts
@@ -1,9 +1,35 @@
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:3031/api/v1/rooms';
+
+  constructor(private http: HttpClient) { }
+
+  getRoom(id: number) : Observable <any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createRoom (room: Object ) : Observable<Object>{
+    return this.http.post(`${this.baseUrl}`, room);
+  }
+
+  updateRoom(id: number, value: any) : Observable<Object>{
+    return this.http.put(`${this.baseUrl}/${id}`, value)
+  }
+
+  deleteRoom(id: number): Observable<any>{
+    return this.http.delete(`${this.baseUrl}/${id}`, {responseType: 'text'});
+  }
+
+  getRoomlist(): Observable<any>{
+    return this.http.get(`${this.baseUrl}`);
+  }
 }
